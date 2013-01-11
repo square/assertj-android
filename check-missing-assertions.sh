ccc61c1980b00796bc14dab20061820bc3a90121
Jake Wharton <jw@squareup.com>
2013-01-11 10:26:32 -0800

    Only check for files which end in 'Assert.java'.

--- check-missing-assertions.sh
+++ check-missing-assertions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ASSERTIONS=`\find src/main/java -type f | \grep -v "/Assertions.java" | \sed 's|src/main/java/||' | \sed 's|\.java||' | \grep -v "/Abstract" | \sed 's|/|\.|g' | \sort`
+ASSERTIONS=`\find src/main/java -type f | \grep "Assert.java" | \sed 's|src/main/java/||' | \sed 's|\.java||' | \grep -v "/Abstract" | \sed 's|/|\.|g' | \sort`
 MISSING=0
 
 for assertion in $ASSERTIONS; do
