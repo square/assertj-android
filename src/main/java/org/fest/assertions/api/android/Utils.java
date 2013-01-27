f815a5867abd0826d58c8ecc2ce845839526e533
Artur Dryomov <artur.dryomov@gmail.com>
2013-01-27 04:31:38 +0300

    Change custom String joiner to use framework one.

--- src/main/java/org/fest/assertions/api/android/Utils.java
+++ src/main/java/org/fest/assertions/api/android/Utils.java
@@ -1,17 +1,11 @@
 package org.fest.assertions.api.android;
 
+import android.text.TextUtils;
 import java.util.List;
 
 public final class Utils {
   public static String join(List<String> parts) {
-    StringBuilder builder = new StringBuilder();
-    for (String part : parts) {
-      if (builder.length() > 0) {
-        builder.append(", ");
-      }
-      builder.append(part);
-    }
-    return builder.toString();
+    return TextUtils.join(", ", parts);
   }
 
   private Utils() {
