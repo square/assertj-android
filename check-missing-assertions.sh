351badcb41836f9bc6829cb9b13f4747ab877d3d
Jake Wharton <jw@squareup.com>
2014-07-11 14:55:55 -0700

    Fix check script for new directory names.

--- check-missing-assertions.sh
+++ check-missing-assertions.sh
@@ -2,7 +2,8 @@
 
 MISSING=0
 
-for project in $(\find . -type d -name 'android*' -maxdepth 1 | \sed 's|./||g'); do
+for project in $(\find . -type d -name 'assertj-android*' -maxdepth 1 | \sed 's|./||g'); do
+  echo "Checking ${project}..."
   ASSERTIONS=$(\find ${project} -name 'Assertions.java')
   CLASSES=$(\find ${project} -type f -name '*Assert.java' | \cut -d'/' -f 5- | \grep -v '/Abstract' | \sed 's|.java||' | \sed 's|/|.|g')
   for class in ${CLASSES}; do
@@ -15,5 +16,7 @@ for project in $(\find . -type d -name 'android*' -maxdepth 1 | \sed 's|./||g');
 done
 
 if [ ${MISSING} -ne 0 ]; then
+  echo "Failed :("
 	exit 1
 fi
+echo "Success!"
