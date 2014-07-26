3ebbb9133b955c94b46cb323f38ceccaa68c0a47
makovkastar <makovkastar@gmail.com>
2014-07-26 17:06:42 +0300

    Add CardView support library module

---
+++ assertj-android-cardview-v7/src/main/java/org/assertj/android/cardview/v7/api/widget/CardViewAssert.java
@@ -0,0 +1,23 @@
+package org.assertj.android.cardview.v7.api.widget;
+
+import android.support.v7.widget.CardView;
+
+import org.assertj.android.api.view.AbstractViewGroupAssert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/** Assertions for {@link android.support.v7.widget.CardView} instances. */
+public class CardViewAssert extends AbstractViewGroupAssert<CardViewAssert, CardView> {
+    public CardViewAssert(CardView actual) {
+        super(actual, CardViewAssert.class);
+    }
+
+    public CardViewAssert hasRadius(int radius) {
+        isNotNull();
+        float actualRadius = actual.getRadius();
+        assertThat(actualRadius) //
+                .overridingErrorMessage("Expected radius <%s> but was <%s>", radius, actualRadius) //
+                .isEqualTo(radius);
+        return this;
+    }
+}
