3091634a946782f2741bf6f285470463d39fa2cf
Jake Wharton <jw@squareup.com>
2014-11-24 10:40:55 -0800

    Fix Assertions file.

--- assertj-android-palette-v7/src/main/java/org/assertj/android/palette/v7/api/Assertions.java
+++ assertj-android-palette-v7/src/main/java/org/assertj/android/palette/v7/api/Assertions.java
@@ -3,8 +3,6 @@
 // This class is generated. Do not modify directly!
 package org.assertj.android.palette.v7.api;
 
-import org.assertj.android.palette.v7.api.graphics.PaletteSwatchAssert;
-
 /** Assertions for testing Android classes. */
 @SuppressWarnings("deprecation")
 public final class Assertions {
@@ -13,9 +11,9 @@ public final class Assertions {
     return new org.assertj.android.palette.v7.api.graphics.PaletteAssert(actual);
   }
 
-  public static PaletteSwatchAssert assertThat(
+  public static org.assertj.android.palette.v7.api.graphics.PaletteSwatchAssert assertThat(
       android.support.v7.graphics.Palette.Swatch actual) {
-    return new PaletteSwatchAssert(actual);
+    return new org.assertj.android.palette.v7.api.graphics.PaletteSwatchAssert(actual);
   }
 
   private Assertions() {
