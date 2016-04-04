f0fb9bd93bae0198f4283aa39b019961d9ab6fa0
Marius Volkhart <Marius@volkhart.com>
2016-04-03 22:56:28 -0500

    Add API 23 assertions to TimePickerAssert
    New assertions for `TimePicker#getHour()` and `TimePicker#getMinute()`.

--- assertj-android/src/main/java/org/assertj/android/api/widget/TimePickerAssert.java
+++ assertj-android/src/main/java/org/assertj/android/api/widget/TimePickerAssert.java
@@ -1,8 +1,11 @@
 // Copyright 2013 Square, Inc.
 package org.assertj.android.api.widget;
 
+import android.annotation.TargetApi;
+import android.support.annotation.IntRange;
 import android.widget.TimePicker;
 
+import static android.os.Build.VERSION_CODES.M;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TimePickerAssert extends AbstractFrameLayoutAssert<TimePickerAssert, TimePicker> {
@@ -24,6 +27,28 @@ public class TimePickerAssert extends AbstractFrameLayoutAssert<TimePickerAssert
     Integer actualMinute = actual.getCurrentMinute();
     assertThat(actualMinute) //
         .overridingErrorMessage("Expected current minute <%s> but was <%s>.", minute,
+            actualMinute) //
+        .isEqualTo(minute);
+    return this;
+  }
+
+  @TargetApi(M)
+  public TimePickerAssert hasHour(@IntRange(from = 0, to = 23) int hour) {
+    isNotNull();
+    int actualHour = actual.getHour();
+    assertThat(actualHour) //
+        .overridingErrorMessage("Expected hour <%s> but was <%s>.", hour,
+            actualHour) //
+        .isEqualTo(hour);
+    return this;
+  }
+
+  @TargetApi(M)
+  public TimePickerAssert hasMinute(@IntRange(from = 0, to = 59) int minute) {
+    isNotNull();
+    int actualMinute = actual.getMinute();
+    assertThat(actualMinute)
+        .overridingErrorMessage("Expected minute <%s> but was <%s>.", minute,
             actualMinute) //
         .isEqualTo(minute);
     return this;
