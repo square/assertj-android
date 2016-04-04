c942bf2175199662f6e887742b6d8debd90c2578
Marius Volkhart <Marius@volkhart.com>
2016-04-04 11:11:05 -0500

    Implement MediaRouteDiscoveryRequestAssert
    Previous to this the class was stubbed.

--- assertj-android-mediarouter-v7/src/main/java/org/assertj/android/mediarouter/v7/api/media/MediaRouteDiscoveryRequestAssert.java
+++ assertj-android-mediarouter-v7/src/main/java/org/assertj/android/mediarouter/v7/api/media/MediaRouteDiscoveryRequestAssert.java
@@ -3,6 +3,8 @@ package org.assertj.android.mediarouter.v7.api.media;
 import android.support.v7.media.MediaRouteDiscoveryRequest;
 import org.assertj.core.api.AbstractAssert;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MediaRouteDiscoveryRequestAssert
     extends AbstractAssert<MediaRouteDiscoveryRequestAssert, MediaRouteDiscoveryRequest> {
   public MediaRouteDiscoveryRequestAssert(MediaRouteDiscoveryRequest actual) {
@@ -11,21 +13,33 @@ public class MediaRouteDiscoveryRequestAssert
 
   public MediaRouteDiscoveryRequestAssert isActiveScan() {
     isNotNull();
+    assertThat(actual.isActiveScan()) //
+        .overridingErrorMessage("is performing active scanning") //
+        .isTrue();
     return this;
   }
 
   public MediaRouteDiscoveryRequestAssert isNotActiveScan() {
     isNotNull();
+    assertThat(actual.isActiveScan()) //
+        .overridingErrorMessage("is not performing active scanning") //
+        .isFalse();
     return this;
   }
 
   public MediaRouteDiscoveryRequestAssert isValid() {
     isNotNull();
+    assertThat(actual.isValid()) //
+        .overridingErrorMessage("Expected to be valid but was not.") //
+        .isTrue();
     return this;
   }
 
   public MediaRouteDiscoveryRequestAssert isNotValid() {
     isNotNull();
+    assertThat(actual.isValid()) //
+        .overridingErrorMessage("Expected to not be valid but was.") //
+        .isFalse();
     return this;
   }
 }
