aaff264bec1bebe796eb36375d83abf95ea24765
Marius Volkhart <Marius@volkhart.com>
2016-04-04 11:01:46 -0500

    Correct the error message for MediaRouteDescriptorAssert#hasVolumeMax(int)

--- assertj-android-mediarouter-v7/src/main/java/org/assertj/android/mediarouter/v7/api/media/MediaRouteDescriptorAssert.java
+++ assertj-android-mediarouter-v7/src/main/java/org/assertj/android/mediarouter/v7/api/media/MediaRouteDescriptorAssert.java
@@ -93,7 +93,7 @@ public class MediaRouteDescriptorAssert
     isNotNull();
     int actualVolumeMax = actual.getVolumeMax();
     assertThat(actualVolumeMax) //
-        .overridingErrorMessage("Expected playback stream <%s> but was <%s>.", volumeMax,
+        .overridingErrorMessage("Expected maximum volume <%s> but was <%s>.", volumeMax,
             actualVolumeMax) //
         .isEqualTo(volumeMax);
     return this;
